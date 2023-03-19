7c5f3e6e3859ceee7c72204e6daa411ccb779232
VladRatautas <127895726+VladRatautas@users.noreply.github.com>
2023-03-20 00:39:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const buttonElem = document.querySelector('.section-button');
+const modalElem = document.querySelector('.modal');
+
+modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 300ms ease-in-out;
+`;
+
+const closeModal = event => {
+    const target = event.target;
+
+    if (target === modalElem || target.closest('.modal-close')) {
+        
+        modalElem.style.opacity = 0;
+
+        setTimeout(() => {
+            modalElem.style.visibility = 'hidden';
+        }, 300)
+    }
+}
+
+const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+};
+
+buttonElem.addEventListener('click', openModal);
+modalElem.addEventListener('click', closeModal);
